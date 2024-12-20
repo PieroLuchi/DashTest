a97580750c0b7c39d9d33b3c424c679a4822d09d
Piero <pieroluchi@hotmail.com>
2024-12-20 10:54:06 +0100

    versione ok

--- src/app.py
+++ src/app.py
@@ -2,16 +2,7 @@ import dash
 from dash import dcc, html, Input, Output
 import requests  # For making API requests
 import dash_bootstrap_components as dbc
-
-names_with_codes = {
-    'HE': 'Hermada',
-    'CE12': 'Checchi',
-    'MDL4': 'Maestri del Lavoro',
-    'CO87': 'Comasina',
-    'ZA15': 'Zanoli',
-    'GRA5': 'Grassini',
-    'EMP1': 'Empoli'
-}
+import json
 
 edifici= {
     'Hermada': 'HE',
@@ -20,48 +11,59 @@ edifici= {
     'Comasina': 'CO87',
     'Zanoli': 'ZA15',
     'Grassini': 'GRA5',
-    'Empoli': 'EMP1'
+    'Empoli': 'EMP1',
+    'Test':'TES0'
 }
 
 
 app = dash.Dash(__name__,external_stylesheets=[dbc.themes.BOOTSTRAP])
+n_clicks_old=0
 server = app.server
+
 app.layout = dbc.Container([
-    html.H1("API Request Interface", className="pad-row"),
+    html.H1("AiEcO Manager", className="pad-row"),
 
     dbc.Row([
-        dbc.Label("Select Edificio"),
-        dbc.Select(
+        dbc.Col(dbc.Label("Seleziona Edificio"),width=2, className="pad-row"),
+        dbc.Col(dbc.Select(
             id='edificio-select',
             options=[{'label': key, 'value': value} for key, value in edifici.items()],
-            value='HE'  # Default value
-        ),
+            value='Test'  # Default value
+        ),width=2, className="pad-row"),
     ]),
     
     dbc.Row([
-        dbc.Col(dbc.Label("Select Option 1"), width=2,className="pad-row"),
+        dbc.Col(dbc.Label("Attivazione"), width=2,className="pad-row"),
         dbc.Col(dbc.Select(
-            id='dropdown-1',
+            id='dropdown-attivazione',
             options=[
-                {'label': 'Option 1', 'value': 'option1'},
-                {'label': 'Option 2', 'value': 'option2'},
-                {'label': 'Option 3', 'value': 'option3'}
+                {'label': 'Attiva!', 'value': "True"},
+                {'label': 'Disattiva!', 'value': "False"}
             ],
-            value='option1',  # Default value
-            ),width=2)
+            value="True",  # Default value
+            ),width=2, className="pad-row")
     ]),
     
     dbc.Row([
-        dbc.Col(dbc.Label("Select Option 2"),width=2),
+        dbc.Col(dbc.Label("Versione Algoritmo"),width=2, className="pad-row"),
         dbc.Col(dbc.Select(
-            id='dropdown-2',
+            id='dropdown-versione',
             options=[
-                {'label': 'Choice A', 'value': 'choiceA'},
-                {'label': 'Choice B', 'value': 'choiceB'},
-                {'label': 'Choice C', 'value': 'choiceC'}
+                {'label': 'Lite', 'value': 'lite'},
+                {'label': 'Standard', 'value': 'standard'}
             ],
-            value='choiceA',  # Default value
-        ),width=2)
+            value='lite',  # Default value
+        ),width=2, className="pad-row")
+    ]),
+
+    dbc.Row([
+        dbc.Col(dbc.Label("Temperatura di Attenuazione"),width=2, className="pad-row"),
+        dbc.Col(dbc.Input(
+                id='input-temperatura',
+                type='text',  # Specify the type of input (text, number, etc.)
+                placeholder='Inserisci temperatura',  # Placeholder text
+                value=50,  # Default value
+                ), width=2, className="pad-row")
     ]),
 
     dbc.Row([
@@ -75,26 +77,34 @@ app.layout = dbc.Container([
 @app.callback(
     Output('response-output', 'children'),
     Input('send-button', 'n_clicks'),
-    Input('dropdown-1', 'value'),
-    Input('dropdown-2', 'value'),
+    Input('edificio-select','value'),
+    Input('dropdown-attivazione', 'value'),
+    Input('dropdown-versione', 'value'),
+    Input('input-temperatura', 'value'),
         prevent_initial_call=True
-
 )
-def send_request(n_clicks, selected_value_1, selected_value_2):
-    if n_clicks > 0:  # Ensure button has been clicked
-        url = "https://api.example.com/endpoint"  # Replace with your API endpoint
-        params = {
-            'param1': selected_value_1,
-            'param2': selected_value_2
-        }
-        
-        # response = requests.get(url, params=params)  # Make the GET request
-        
-        # if response.status_code == 200:
-        #     return f"Response: {response.json()}"  # Display response data
-        # else:
-        #     return f"Error: {response.status_code} - {response.text}"
-        return "mandato"
+def send_request(n_clicks, nome_edificio,stato_attivazione, versione,temp_attivazione):
+    global n_clicks_old
+    if n_clicks!=None:
+        if n_clicks>n_clicks_old:  # Ensure button has been clicked
+
+            n_clicks_old=n_clicks
+
+            params = {
+                'nome_edificio': nome_edificio,
+                'stato_attivazione': stato_attivazione,
+                'versione':versione,
+                'temp_attivazione':temp_attivazione
+            }
+
+            url='https://abitarefunctionapp.azurewebsites.net/api/AiEcoStateSelector?'
+            
+            response = requests.get(url, params=params)  # Make the GET request
+            # if response.status_code == 200:
+            #     return f"Response: {json.dumps(params)}"  # Display response data
+            # else:
+            #     return f"Error: {response.status_code}"
+            return json.dumps(params)
 
 # Run the app
 if __name__ == '__main__':
